70c0a5da4e60a6dc6404b8c7909bab2947305b04
aparna60 <abhattacharya60@gmail.com>
2025-08-18 10:58:58 +0530

    Swiper demo initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { register as swiperRegister } from 'swiper/element/bundle';
 
+
+// Register Swiper elements
+swiperRegister();
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'SwiperDemo';
